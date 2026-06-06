740f7df9291e4fbe21c6687578c3ed45b68a6482
ponseham <ponseha94xgmail.com>
2026-06-06 18:22:18 +0530

    Show books information as card

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -9,4 +9,16 @@ describe('Book Store', () => {
     test('Show book store header', () => {
         expect(screen.getByText(CONSTANTS.STORE_HEADER_TITLE)).toBeInTheDocument()
     })
+    test('Show books information as card', () => {
+        CONSTANTS.BOOKS.forEach(book => {
+            const image = screen.getByAltText(book.title)
+            expect(image.src).toContain('/images')
+            expect(image).toBeInTheDocument()
+            expect(image).toHaveAttribute('src', book.coverUrl)
+            expect(screen.getByText(book.title)).toBeInTheDocument()
+            expect(screen.getAllByText(book.author)).toHaveLength(CONSTANTS.BOOKS_BY_SAME_AUTHOR[book.author])
+        })
+        const prices = screen.getAllByText(`${CONSTANTS.BOOK_PRICE} ${CONSTANTS.CURRENCY_LABEL}`)
+        expect(prices).toHaveLength(CONSTANTS.BOOKS.length)
+    })
 })
