18cf9aff0ffec0483caa6a63228f3ee158ce4d6f
ponseham <ponseha94@gmail.com>
2026-06-10 14:42:14 +0530

    Display quantities of books and pricing summary when adding more than one sets of books

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,5 +1,5 @@
-import { useDispatch } from 'react-redux'
-import { TEST_ID_BASKET_ITEM, REMOVE_BUTTON_LABEL } from '../constants/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { TEST_ID_BASKET_ITEM, REMOVE_BUTTON_LABEL, BOOK_QUANTITY_SEPARATOR } from '../constants/constants'
 import { REMOVE_BUTTON_ARIA_LABEL } from '../constants/ariaConstants'
 import { removeBookFromBasket } from '../store/actions'
 
@@ -10,10 +10,12 @@ export default function BasketItem({ book }) {
     function handleRemoveBook() {
         dispatch(removeBookFromBasket(book.id))
     }
+    const quantity = useSelector((state) => state.items[book.id])
     return (
         <div className="basket-item" data-testid={TEST_ID_BASKET_ITEM}>
             <span>
                 {book.title}
+                <span className="quantity-badge">{BOOK_QUANTITY_SEPARATOR}{quantity}</span>
             </span>
             <button
                 className="remove-button"
--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -121,4 +121,12 @@ describe('Book Store', () => {
         expect(screen.getByTestId(CONSTANTS.TEST_ID_SUBTOTAL)).toHaveTextContent(CONSTANTS.FIVE_BOOK_WITH_DISCOUNT_SUBTOTAL)
         expect(screen.getByTestId(CONSTANTS.TEST_ID_DISCOUNT)).toHaveTextContent(CONSTANTS.FIVE_BOOK_WITH_DISCOUNT_DISCOUNT)
     })
+    test("Display quantities of books and pricing summary when adding more than one sets of books", async () => {
+        await addGivenBooksToBasket([0, 1, 2, 3, 4, 1, 2])
+        expect(screen.getByTestId(CONSTANTS.TEST_ID_TOTAL)).toHaveTextContent(CONSTANTS.SEVEN_BOOK_WITH_DISCOUNT_TOTAL)
+        expect(screen.getByTestId(CONSTANTS.TEST_ID_SUBTOTAL)).toHaveTextContent(CONSTANTS.SEVEN_BOOK_WITH_DISCOUNT_SUBTOTAL)
+        expect(screen.getByTestId(CONSTANTS.TEST_ID_DISCOUNT)).toHaveTextContent(CONSTANTS.SEVEN_BOOK_WITH_DISCOUNT_DISCOUNT)
+        expect(screen.getAllByText(CONSTANTS.BOOK_QUANTITY_SEPARATOR + '1')).toHaveLength(3)
+        expect(screen.getAllByText(CONSTANTS.BOOK_QUANTITY_SEPARATOR + '2')).toHaveLength(2)
+    })
 })
