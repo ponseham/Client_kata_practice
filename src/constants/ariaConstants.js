9d9e70f09f998bac822e59e1354bfcab2ef7166f
ponseham <ponseha94@gmail.com>
2026-06-10 17:04:45 +0530

    Update basket item quantity when adding and removing copies

--- src/constants/ariaConstants.js
+++ src/constants/ariaConstants.js
@@ -1,3 +1,4 @@
 export const ADD_TO_BASKET_ARIA_LABEL = 'Add _ to basket'
 export const REMOVE_BUTTON_ARIA_LABEL = 'Remove _'
 export const CLEAR_BASKET_ARIA_LABEL = 'Clear basket'
+export const REMOVE_SINGLE_BOOK_FROM_BASKET_ARIA_LABEL = 'Remove one copy of _ from basket'
