4c28fac36e9a615a7facff1d764454df792e737c
ponseham <ponseha94@gmail.com>
2026-06-11 06:54:56 +0530

    Refactor the code replace if/else chain with constant

--- src/utils/priceCalculation.js
+++ src/utils/priceCalculation.js
@@ -1,21 +1,22 @@
 import { BOOK_PRICE } from '../constants/books'
+import { DISCOUNT_RATES } from '../constants/discount'
+import { NO_DISCOUNT } from '../constants/constants'
 
 export function calculateBasketPrice(basketItems) {
     let total = 0
     let subtotal = 0
     let basketSize = Object.keys(basketItems).length
 
-    subtotal = basketSize * BOOK_PRICE
-    let discount = 0
-    if (basketSize === 2)
-        discount = 0.05
-    else if (basketSize === 3)
-        discount = 0.10;
-    else if (basketSize === 4)
-        discount = 0.20;
-    else if (basketSize === 5)
-        discount = 0.25;
-    total = basketSize * BOOK_PRICE * (1 - discount)
+    subtotal = calculatePriceForBasket(basketSize)
+    let discountRate = calculateDiscountForBasket(basketSize)
+    total = calculatePriceForBasket(basketSize) * (1 - discountRate)
 
     return { subtotal, discount: subtotal - total, total }
+}
+function calculatePriceForBasket(basketSize) {
+    return basketSize * BOOK_PRICE
+}
+
+function calculateDiscountForBasket(basketSize) {
+    return DISCOUNT_RATES.get(basketSize) ?? NO_DISCOUNT
 }
